319a93599ca874de5547381a764bde5581e58729
mloeraschz <bio.mloera@gmail.com>
2016-04-09 16:06:45 -0500

    Create 8-figuras.py

---
+++ 8-figuras.py
@@ -0,0 +1,37 @@
+## Instalar PIL
+## Instalar reportlab
+
+from reportlab.lib import colors
+from reportlab.lib.units import cm
+from Bio.Graphics import GenomeDiagram
+from Bio import SeqIO
+
+
+elemento = SeqIO.parse('pon tu archivo genbank o multigenebank','genbank')
+
+for x in elemento:
+    print(x.id)
+    #print(x.description)
+    #print(x.features)
+    for y in x.features:
+        print(y.type)
+        gd_diagram = GenomeDiagram.Diagram(x.id)
+        gd_track_for_features = gd_diagram.new_track(1, name="Annotated Features")
+        gd_feature_set = gd_track_for_features.new_set()
+
+        if y.type != "gene":
+        #Exclude this feature
+            continue
+        if len(gd_feature_set) % 2 == 0:
+            color = colors.blue
+        else:
+            color = colors.lightblue
+        gd_feature_set.add_feature(y, color=color, label=True,sigil="ARROW",arrowshaft_height=1.0)
+        gd_diagram.draw(format="linear", orientation="landscape", pagesize='A4',
+                    fragments=4, start=0, end=len(x))
+        
+        gd_diagram.write("%s.png"%x.id, "PNG")
+            
+        
+    
+
